c33b8e093877a13e681adf624efd2a2e72f3af3d
ddkeating <73508313+ddkeating@users.noreply.github.com>
2023-09-27 12:20:41 +1300

    Added mapping

--- VariableLearning.js
+++ VariableLearning.js
@@ -9,6 +9,7 @@ let salary = 2415.85; // Defined in local scope.
 
 var grade = 78.43; // Defined in global scope.
 
+// Data types
 let colors = []
 
 console.log(colors);
@@ -19,6 +20,7 @@ colors.push("Red");
 
 console.log('', ...colors)
 
+// Arrow function
 const numbers = [23, 45, 68, 12, 34, 54, 78, 90];
 const filterNumbers = numbers.filter(number => number % 2 === 0);
 console.log(filterNumbers);
@@ -26,3 +28,23 @@ console.log(filterNumbers);
 const names = ['Jim', 'Bob', 'Sarah', 'Alex', 'James', 'Sam', 'Peter']
 const filterNames = names.filter(name => name.startsWith("S"))
 console.log(filterNames)
+
+// Maps and Sets - Syntax
+
+const hogwartsStudents = new Map(
+    [
+        ["Harry Potter", "Gryffindor"],
+        ["Draco Malfoy", "Slytherin"],
+        ["Luna Lovegood", "Ravenclaw"]
+    ]
+);
+
+console.log(hogwartsStudents.get("Harry Potter"));
+
+console.log(hogwartsStudents.set("Hermione Granger", "Gryffindor"));
+
+console.log(hogwartsStudents.has("Luna Lovegood"));
+
+console.log(hogwartsStudents.size);
+
+console.log(hogwartsStudents.delete("Draco Malfoy"));
