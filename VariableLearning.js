e4c100d398226d919e95d6dd008bf781a9fab2a0
ddkeating <73508313+ddkeating@users.noreply.github.com>
2023-09-27 12:03:35 +1300

    Update VariableLearning.js

--- VariableLearning.js
+++ VariableLearning.js
@@ -4,3 +4,17 @@ let numberOfPieces = 8;
 
 const taxNumber = "12-15-21-12";
 console.log(favoriteFood)
+
+let salary = 2415.85; // Defined in local scope.
+
+var grade = 78.43; // Defined in global scope.
+
+let colors = []
+
+console.log(colors);
+
+colors.push("Brown");
+colors.push("Green");
+colors.push("Red");
+
+console.log('', ...colors)
