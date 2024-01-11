066ee1393c6b58c87d0d67c94140d2f44b1baa98
ddkeating <73508313+ddkeating@users.noreply.github.com>
2024-01-11 13:08:35 +1300

    Guessing Game created using the react library

--- React/react-hooks/src/index.js
+++ React/react-hooks/src/index.js
@@ -1,17 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useRef, useEffect, useState, useReducer } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function NumberGenerator() {
+	//Testing to see if math.random works
+	return Math.floor(Math.random() * 9);
+}
+
+const NumberButton = [];
+
+for (let i = 0; i < 10; i++) {
+	NumberButton.push(i);
+}
+
+function App() {
+	const [number, setNumber] = useState(0);
+	const [randomNumber, setRandomNumber] = useState(0);
+	const [result, setResult] = useState(null);
+
+	useEffect(() => {
+		setRandomNumber(NumberGenerator());
+	}, []);
+
+	const GuessNumber = () => {
+		setResult(randomNumber === number ? "Correct" : "Incorrect");
+		setRandomNumber(NumberGenerator());
+	};
+
+	return (
+		<div className="Main">
+			<h1>Simple Guessing Game</h1>
+			<div className="Button-Container">
+				{NumberButton.map((number) => (
+					<button
+						className="Random-Button"
+						key={number}
+						onClick={() => setNumber(number)}
+					>
+						{console.log(randomNumber)}
+						{number}
+					</button>
+				))}
+			</div>
+			<button onClick={GuessNumber}>Guess</button>
+			{result && <h1>{result}</h1>}
+		</div>
+	);
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
